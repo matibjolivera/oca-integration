52c80fd8bbb3a4128d35e9679043f71dc74913ee
Matías Olivera <matibjolivera@gmail.com>
2022-10-21 21:27:02 -0300

    get offices

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const OCA_URL = 'http://webservice.oca.com.ar/oep_tracking/Oep_Track.asmx/'
 
 app.get('/api/provinces', async (req, res) => {
     let response = await axios.get(OCA_URL + 'GetProvincias');
-    const provinces = await conversion(response.data)
+    res.json(await conversion(response.data))
 })
+
+app.get('/api/offices', async (req, res) => {
+    let response = await axios.get(OCA_URL + 'GetServiciosDeCentrosImposicion');
+    res.json(await conversion(response.data))
+})
+
 
 app.listen(3000, () => {
     console.log(`Example app listening on port 3000`)
