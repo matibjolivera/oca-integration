66942699c566d661493840d4567e84df3fc9662b
Matías Olivera <matibjolivera@gmail.com>
2022-10-22 18:44:03 -0300

    estimate shipping cost

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ const conversion = require('./helpers/xml_to_json')
 const OCA_URL = 'http://webservice.oca.com.ar/oep_tracking/Oep_Track.asmx/'
 
 app.get('/api/provinces', async (req, res) => {
-    let response = await axios.get(OCA_URL + 'GetProvincias');
+    let response = await axios.get(`${OCA_URL}GetProvincias`);
     res.json(await conversion(response.data))
 })
 
 app.get('/api/offices', async (req, res) => {
-    let response = await axios.get(OCA_URL + 'GetServiciosDeCentrosImposicion');
+    let response = await axios.get(`${OCA_URL}GetServiciosDeCentrosImposicion`);
     res.json(await conversion(response.data))
 })
 
 app.get('/api/shipping_status', async (req, res) => {
-    let response = await axios.get(OCA_URL + 'TrackingEnvio_EstadoActual?numeroEnvio=' + req.query.tracking_number);
+    let response = await axios.get(`${OCA_URL}TrackingEnvio_EstadoActual?numeroEnvio=${req.query.tracking_number}`);
+    res.json(await conversion(response.data))
+})
+
+app.get('/api/estimate_shipping_cost', async (req, res) => {
+    let response = await axios.get(`${OCA_URL}Tarifar_Envio_Corporativo?PesoTotal=${req.query.weight}&VolumenTotal=${req.query.volume}&CodigoPostalOrigen=${req.query.zip_code_origin}&CodigoPostalDestino=${req.query.zip_code_destination}&Cuit=${req.query.cuit}&Operativa=${req.query.operative}&CantidadPaquetes=1`);
     res.json(await conversion(response.data))
 })
 
