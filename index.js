be1d571f4e8c45b83f4a010c228a86104136ae7d
Matías Olivera <matibjolivera@gmail.com>
2022-10-31 14:40:06 -0300

    use new api epak

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const axios = require('axios')
 const conversion = require('./helpers/xml_to_json')
 
-const OCA_URL = 'http://webservice.oca.com.ar/oep_tracking/Oep_Track.asmx/'
+const OCA_URL = 'http://webservice.oca.com.ar/ePak_tracking/Oep_TrackEPak.asmx/'
 
 app.get('/api/provinces', async (req, res) => {
     let response = await axios.get(`${OCA_URL}GetProvincias`);
@@ -11,17 +11,17 @@ app.get('/api/provinces', async (req, res) => {
 })
 
 app.get('/api/offices', async (req, res) => {
-    let response = await axios.get(`${OCA_URL}GetServiciosDeCentrosImposicion`);
+    let response = await axios.get(`${OCA_URL}GetCentrosImposicion`);
     res.json(await conversion(response.data))
 })
 
 app.get('/api/shipping_status', async (req, res) => {
-    let response = await axios.get(`${OCA_URL}TrackingEnvio_EstadoActual?numeroEnvio=${req.query.tracking_number}`);
+    let response = await axios.get(`${OCA_URL}Tracking_PiezaNumeroEnvio?Pieza=${req.query.tracking_number}`);
     res.json(await conversion(response.data))
 })
 
 app.get('/api/estimate_shipping_cost', async (req, res) => {
-    let response = await axios.get(`${OCA_URL}Tarifar_Envio_Corporativo?PesoTotal=${req.query.weight}&VolumenTotal=${req.query.volume}&CodigoPostalOrigen=${req.query.zip_code_origin}&CodigoPostalDestino=${req.query.zip_code_destination}&Cuit=${req.query.cuit}&Operativa=${req.query.operative}&CantidadPaquetes=1`);
+    let response = await axios.get(`${OCA_URL}Tarifar_Envio_Corporativo?PesoTotal=${req.query.weight}&VolumenTotal=${req.query.volume}&CodigoPostalOrigen=${req.query.zip_code_origin}&CodigoPostalDestino=${req.query.zip_code_destination}&Cuit=${req.query.cuit}&Operativa=${req.query.operative}&CantidadPaquetes=1&ValorDeclarado=${req.query.declarated_value}`);
     res.json(await conversion(response.data))
 })
 
